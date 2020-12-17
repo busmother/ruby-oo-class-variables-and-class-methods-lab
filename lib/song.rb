32fb8cec2c4f3c841dbe751379603b602b04b3f0
Ian Marshall <imarshall14@gmail.com>
2020-12-17 16:58:10 -0500

    commit tree 04346d938fcbab0b01b8a6988005f17e523eb9b9

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    def initialize(name, artist, genre)
+        @@count += 1
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists << @artist
+        @@genres << @genre
+    end
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.genre_count
+        @@genre_count = {}
+        @@genres.group_by(&:itself).each {|key, value| @@genre_count[key]=value.count}
+        return @@genre_count
+    end
+    def self.artist_count
+        @@artist_count = {}
+        @@artists.group_by(&:itself).each {|key, value| @@artist_count[key]=value.count}
+        return @@artist_count
+    end
+end
